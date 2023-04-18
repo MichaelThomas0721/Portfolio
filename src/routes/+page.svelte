65e7dbfdbbf21698ecb3a52816602c2978691907
Michael Thomas <michaelthomas0721@icloud.com>
2023-04-18 15:09:35 -0400

    Feat: Added meta data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,30 @@
 <script>
   import ContactSection from "$components/Contact/ContactSection.svelte";
-import ExperienceSection from "$components/Experience/ExperienceSection.svelte";
+  import ExperienceSection from "$components/Experience/ExperienceSection.svelte";
   import Footer from "$components/Footer/Footer.svelte";
-import IntroSection from "$components/Intro/IntroSection.svelte";
+  import IntroSection from "$components/Intro/IntroSection.svelte";
   import ProjectsSection from "$components/Projects/ProjectsSection.svelte";
   import SkillsSection from "$components/Skills/SkillsSection.svelte";
   export let data;
   const { text } = data;
 </script>
 
-  <IntroSection />
-  <SkillsSection />
-  <ProjectsSection />
-  <ExperienceSection />
-  <ContactSection />
-  <Footer />
+<head>
+  <title>Michael Thomas - Portfolio</title>
+  <meta name="description" content="Michael Thomas - Portfolio" />
+  <meta name="keywords" content="Michael Thomas, Portfolio" />
+  <meta name="author" content="Michael Thomas" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#ffffff" />
+  <link rel="icon" type="image/png" href="/favicon.png" />
+</head>
+
+<IntroSection />
+<SkillsSection />
+<ProjectsSection />
+<ExperienceSection />
+<ContactSection />
+<Footer />
 
 <style lang="postcss">
   :global(html) {
